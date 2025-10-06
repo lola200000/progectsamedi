505d61f86240dc1d833e8779de6710a9c13b95d1
lola200000 <mirzoyanlola@gmail.com>
2025-10-06 09:19:48 +0200

    Update UserController.php

--- controller/UserController.php
+++ controller/UserController.php
@@ -2,7 +2,7 @@
 require_once 'model/User.php';
 
 class UserController {
-    public function show() {
+    public function showUser() {
         $user = new User("Jean Dupont");
         require 'view/userView.php';
     }
