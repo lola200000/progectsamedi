96d16e0b46220732a837218fbfd9c93dfe9d7e11
lola200000 <mirzoyanlola@gmail.com>
2025-10-07 15:49:54 +0200

    Update and rename listproduct.php to footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Mini projet PDO</title>
+  <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Bienvenue dans le super exercice</h1>
+    <hr>
