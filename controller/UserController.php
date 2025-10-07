a78c3b6045d5c2307b528d5a600738bb0067d833
lola200000 <mirzoyanlola@gmail.com>
2025-10-07 15:41:04 +0200

    Update UserController.php

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,9 +1,14 @@
 <?php
-require_once 'model/User.php';
+//require_once __DIR__ . '/../model/user.php';
 
-class UserController {
-    public function showUser() {
-        $user = new User("Jean Dupont");
-        require 'view/userView.php';
-    }
-}
+//class UserController {
+//   public static function list() {
+//        $people = getAllPeople(); 
+//        require __DIR__ . '/../view/listpeople.php';
+//    }
+
+//    public static function view(int $id) {
+//        $person = getPersonById($id);
+//        require __DIR__ . '/../view/viewpeople.php';
+//    }
+//}
