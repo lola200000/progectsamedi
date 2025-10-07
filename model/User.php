42138b4da5d75c618dd0ab9d7a52c902720acbfa
lola200000 <mirzoyanlola@gmail.com>
2025-10-07 15:42:01 +0200

    Update User.php

--- model/User.php
+++ model/User.php
@@ -1,10 +1,16 @@
 <?php
+require_once __DIR__ . '/bd.php';
+$pdo = DataBase::getConnection();
 
+function getAllPeople() {
+    global $pdo;
+    $stmt = $pdo->query("SELECT * FROM people");
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
-class User {
-    public  $name;
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
+function getPersonById($id) {
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT * FROM people WHERE id = ?");
+    $stmt->execute([$id]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
