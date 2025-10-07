ab0a42759ab8a7b25b13d57a37495c172a3914cb
lola200000 <mirzoyanlola@gmail.com>
2025-10-07 15:46:13 +0200

    Update and rename view/userView.php to view.

--- view/userView.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($user)) {
-    echo "<p style='color:red'>Erreur : utilisateur non trouvé.</p>";
-} else {
-    echo "<h1>Utilisateur : " . htmlspecialchars($user->name, ENT_QUOTES, 'UTF-8') . "</h1>";
-}
-?>
