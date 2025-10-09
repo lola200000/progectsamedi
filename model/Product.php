1a3662ebbe0488b287fd5f91047392022f278ce3
lola200000 <mirzoyanlola@gmail.com>
2025-10-09 11:51:35 +0200

    Update Product.php

--- model/Product.php
+++ model/Product.php
@@ -1,40 +1,16 @@
 <?php
-require_once __DIR__ . '/bd.php'; 
-$pdo = DataBase::getConnection(); 
 
+class Product {
 
-function getAllProducts(): array {
-    global $pdo;
-    $stmt = $pdo->query("SELECT * FROM products");
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public int $id;
+    
+    public string $name;
+    
+
+    public function __construct($id,  $name ) {
+        $this->id = $id; 
+        $this->name = $name;
+        
+    }
 }
 
-
-function getProductById(int $id): ?array {
-    global $pdo;
-    $stmt = $pdo->prepare("SELECT * FROM products WHERE id = ?");
-    $stmt->execute([$id]);
-    $product = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $product ?: null;
-}
-
-
-function addProduct(string $name): bool {
-    global $pdo;
-    $stmt = $pdo->prepare("INSERT INTO products (name) VALUES (?)");
-    return $stmt->execute([$name]);
-}
-
-
-function updateProduct(int $id, string $name): bool {
-    global $pdo;
-    $stmt = $pdo->prepare("UPDATE products SET name = ? WHERE id = ?");
-    return $stmt->execute([$name, $id]);
-}
-
-
-function deleteProduct(int $id): bool {
-    global $pdo;
-    $stmt = $pdo->prepare("DELETE FROM products WHERE id = ?");
-    return $stmt->execute([$id]);
-}
