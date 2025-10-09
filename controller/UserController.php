33a144aa7ae0a8f464240f4e6402351d32469f11
lola200000 <mirzoyanlola@gmail.com>
2025-10-09 11:50:30 +0200

    Update UserController.php

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,14 +1,52 @@
 <?php
-//require_once __DIR__ . '/../model/user.php';
+require_once __DIR__ . '/../model/dao/UserDao.php';
+require_once __DIR__ . '/../model/User.php';
 
-//class UserController {
-//   public static function list() {
-//        $people = getAllPeople(); 
-//        require __DIR__ . '/../view/listpeople.php';
-//    }
+class UserController {
 
-//    public static function view(int $id) {
-//        $person = getPersonById($id);
-//        require __DIR__ . '/../view/viewpeople.php';
-//    }
-//}
+    public $userDao;
+
+    public function __construct($userDao) {
+        $this->userDao = $userDao;
+    }
+
+    public function displayAllUsers() {
+        $users = $this->userDao->getAllUsers();
+        require_once __DIR__ . '/../view/UsersView.php';
+    }
+
+     public function displayOneUser() {
+            $id = $_GET['id'] ?? null;
+            if ($id) {
+            $userData = $this->userDao->getUserById($id);
+            require "view/UserView.php";
+        }   
+    }
+
+    public function addUser() {
+        $name = $_POST['name'] ?? '';
+        $age = $_POST['age'] ?? 0;
+        $this->userDao->addUser($name, $age);
+        header("Location: index.php?action=users");
+        exit;
+    }
+
+    public function deleteUser() {
+        $id = $_POST['deleteuser'] ?? null;
+        if ($id) {
+        $this->userDao->deleteUser($id);
+        }
+        header("Location: index.php?action=users");
+        exit;
+    }
+
+    public function updateUser() {
+        $id = (int) ($_POST['updateuser'] ?? 0);
+        $name = $_POST['name'] ?? '';
+        $age = $_POST['age'] ?? 0;
+        $this->userDao->updateUser($id, $name, $age);
+        header("Location: index.php?action=users");
+        exit;
+    }
+
+}
