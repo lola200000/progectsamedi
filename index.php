fba19c97ed54d8c398348c6d637ee135789d6110
lola200000 <mirzoyanlola@gmail.com>
2025-10-09 12:04:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,73 @@
 <?php
-//require_once __DIR__ . '/user.php';
-//require_once __DIR__ . '/bd.php';
-//require_once __DIR__ . '/products.php';
 
-//$page = $_GET['page'] ?? 'people';
-//$id   = $_GET['id'] ?? null;
+require_once __DIR__ . '/controller/UserController.php';
+require_once __DIR__ . '/controller/ProductController.php';
+require_once __DIR__ . '/model/dao/bd.php';
+require_once __DIR__ . '/model/dao/UserDao.php';
+require_once __DIR__ . '/model/dao/ProductDao.php';
 
-//if ($page === 'people') {
-//    $people = getAllPeople($pdo);
-//    require __DIR__ . '/listpeople.php';
-//} elseif ($page === 'person' && $id) {
-//    $person = getPersonById($pdo, $id);
-//   require __DIR__ . '/viewpeople.php';
-//} elseif ($page === 'products') {
-//    $products = getAllProducts($pdo);
-//    require __DIR__ . '/listproduct.php';
-//} else {
-//  require __DIR__ . '/+/header.php';
-//   echo "<p>Page non trouvée.</p>";
-//   require __DIR__ . '/+/footer.php';
-//}
+$action = $_GET['action'] ?? "users";
+
+$pdo = BaseDeDonnees::getConnexion();
+
+$productDao = new ProductDao($pdo);
+$productController = new ProductController($productDao);
+$products = $productDao->getAllProducts();
+
+$userDao = new UserDao($pdo);
+$userController = new UserController($userDao);
+$users = $userDao->getAllUsers();
+
+
+
+// var_dump($users);
+
+switch ($action) {
+    case 'users':
+      $userController->displayAllUsers();
+      break;
+
+    case 'userview' :
+      $userController->displayOneUser();
+      break; 
+        
+    case 'adduser':
+      $userController->addUser();
+      break;
+
+    case 'deleteuser':
+      $userController->deleteUser();
+      break;
+
+    case 'updateuser':
+      $userController->updateUser();
+      break;
+    
+    case 'products':
+      $productController->displayAllProducts();
+      break;
+      
+    case 'productview' :
+          $productController->displayOneProduct();
+          break; 
+
+    case 'deleteproduct':
+      $productController->deleteProduct();
+      break;
+
+    case 'addproduct':
+      $productController->addProduct();
+      break;
+
+    case 'updateproduct':
+    $productController->updateProduct();
+    break;
+
+    default:
+        http_response_code(404);
+        exit;
+}
+
+
+
+?>
